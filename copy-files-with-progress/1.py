229ddba3ad40b8e3355b5f8fdeab9c55ea519ec5
rave974 <87599198+rave974@users.noreply.github.com>
2022-01-27 22:53:35 +0200

    Update and rename copy-files-with-progress.py to copy-files-with-progress/1.py

--- copy-files-with-progress/1.py
+++ copy-files-with-progress/1.py
@@ -1,50 +1,42 @@
 #copy a directory or a file to other location and display copied amount
 #one thread copies file by file, other thread measures file size in parallel to the first thread and it displays each second
-
 import os, time, shutil, sys, random, threading
 
-#nu scrie "/" la final!
+#dont write '/' at the end
 s = '/media/user/disc/test1'
 d = '/media/user/disc/test2'
 
 index_rel = s.rfind('/') + 1
-
 uf = ''
 
 def f1():
 	global uf
 	m_t = 0 #marime total
 	m_f = 0 #marime f
-
-	for x, y, z in os.walk(s): #modul py
+	
+	for x, y, z in os.walk(s):
 		rel = x[index_rel:]; rel = os.path.join(d, rel)
 		for f in z:
 			f_sursa, f_dest = os.path.join(x, f), os.path.join(rel, f)
-			
-			while 1: #daca uf e gol
+			while 1:
 				if uf: break
 				else: time.sleep(1)	
 	
-	
 			while 1:
-			
 				while 1:
 					try: m_f = os.path.getsize(f_dest); break
-					except: pass
-						
+					except: pass	
 				if uf == f_dest:
 					sys.stdout.write('\r\033[K' + str(m_t + m_f)) #modul py
 					time.sleep(1)
-					
 				elif not uf:
 					print()
 					print('final', m_t)
 					exit()
-
 				else:
 					m_t += m_f
 					break
-			
+					
 def f2():
 	global uf
 	for x, y, z in os.walk(s): #modul py
